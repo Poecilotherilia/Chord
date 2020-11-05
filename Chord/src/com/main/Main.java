a7081c96d89c27729fd8a6c88f26db89aa70fcf4
unknown <laijiajie@163.com>
2020-11-05 14:09:01 +0800

    save

--- Chord/src/com/main/Main.java
+++ Chord/src/com/main/Main.java
@@ -15,25 +15,25 @@ import com.Net.Sever;
 import com.alibaba.fastjson.*;
 
 public class Main {
-	static Method event ;
-	
-	public static void main(String[] args) throws Exception {
-
-        InetAddress addr = InetAddress.getLocalHost();
-        System.out.println("Local HostAddress: "+addr.getHostAddress() +" "+ Const.RING_LOC_LIST[2]);
-        Sever sever = new Sever();
-	    Thread t = new Thread(sever);
-	    t.start();
-	    
-	    ScannerTest test = new ScannerTest();
-	    Thread t1 = new Thread(test);
-	    t1.start();
-	    
-        Node.CreateNewNode("127.0.0.1",Const.PORT,"127.0.0.1:15698");
-        event = Node.class.getMethod("a", String.class);
-        event.invoke(Node.Instance(), "aaa");
-        Node.Instance().FindFile(fileLoc, onFindFile, instance);
-    }
-	
+//	static Method event ;
+//	
+//	public static void main(String[] args) throws Exception {
+//
+//        InetAddress addr = InetAddress.getLocalHost();
+//        System.out.println("Local HostAddress: "+addr.getHostAddress() +" "+ Const.RING_LOC_LIST[2]);
+//        Sever sever = new Sever();
+//	    Thread t = new Thread(sever);
+//	    t.start();
+//	    
+//	    ScannerTest test = new ScannerTest();
+//	    Thread t1 = new Thread(test);
+//	    t1.start();
+//	    
+//        Node.CreateNewNode("127.0.0.1",Const.PORT,"127.0.0.1:15698");
+//        event = Node.class.getMethod("a", String.class);
+//        event.invoke(Node.Instance(), "aaa");
+//        Node.Instance().FindFile(fileLoc, onFindFile, instance);
+//    }
+//	
 
 }
